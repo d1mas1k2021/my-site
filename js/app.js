339f78415efd7d30f8d64c460119a9e14fb516dc
d1mas1k2021 <d1mas1k2KA@yandex.ru>
2022-03-10 17:43:48 +0300

    form

--- js/app.js
+++ js/app.js
@@ -1,2 +1,38 @@
+// Navigation
+// Ссылки
+const linkGlav = document.querySelectorAll('.link')[0]
+const linkSummary = document.querySelectorAll('.link')[1]
+const linkPortfolio = document.querySelectorAll('.link')[2]
+const linkShop = document.querySelectorAll('.link')[3]
 
+// Резюме
+const summary = document.querySelector('.information')
+linkSummary.addEventListener('click', e => {
+    e.preventDefault()
+    summary.scrollIntoView({
+        block: 'center',
+        inline: 'center',
+        behavior: 'smooth'
+    })
+})
+// Портфолио
+const portfolio = document.querySelector('.works')
+linkPortfolio.addEventListener('click', e => {
+    e.preventDefault()
+    portfolio.scrollIntoView({
+        block: 'center',
+        inline: 'center',
+        behavior: 'smooth'
+    })
+})
+// Магазин
+
+linkShop.addEventListener('click', e => {
+    e.preventDefault()
+    summary.scrollIntoView({
+        block: 'center',
+        inline: 'center',
+        behavior: 'smooth'
+    })
+})
 
